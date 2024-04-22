777d4a0ceca6810543a27937708f41f49e543e7b
Abenezer Kindie <abenij09@gmail.com>
2024-04-22 09:03:58 +0300

    changed the icon and site name

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - millionair',
-    title: 'millionair',
+    titleTemplate: '%s - Derash',
+    title: 'Derash',
     htmlAttrs: {
       lang: 'en',
     },
@@ -17,7 +17,9 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon.png' }],
+  
+    // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
